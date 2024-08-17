20a82efe1684329647944adeaa5cadf8f124dd07
husham <hishamalmged@gmail.com>
2024-08-16 18:55:16 -0700

    lesson-3

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
   res.sendFile('./views/home.html',{root: __dirname})
 })
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
+
+mongoose.connect('mongodb+srv://husham:H2113993881@cluster0.dlfcz.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0')
+.then(()=>{
+    app.listen(port, () => {
+        console.log(`http://localhost:${port}/`)
+    })
 })
+.catch((err)=>{console.log(err)});
